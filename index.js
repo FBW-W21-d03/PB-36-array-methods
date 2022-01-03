85227aec45516a5825aa6d79ad05c49bf0b49993
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-03 10:23:00 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,14 @@
+// TEIL-1
+const sprachen =["Deutsch" ,"Englisch" ,"Französisch" ,"Italienisch","Russisch", "Arabisch", "Portugiesisch"];
+
+// TEIL-2
+sprachen.sort();
+
+console.log(sprachen);
+
+// TEIL-3
+sprachen.sort();
+
+sprachen.reverse();
+
+console.log(sprachen);
