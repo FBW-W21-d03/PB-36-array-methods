d6d0163e68717d01b6b84681ce72de693e534cc4
kawanaza <kawanbabani@gmail.com>
2022-01-03 10:23:57 +0100

    Done

---
+++ main.js
@@ -0,0 +1,15 @@
+const NamenLanguages = [
+  "Deutsch",
+  "Englisch",
+  "Spanisch",
+  "Französisch",
+  "Italienisch",
+  "Russisch",
+  "Arabisch",
+  "Portugiesisch",
+];
+NamenLanguages.sort();
+console.log(NamenLanguages);
+
+NamenLanguages.reverse();
+console.log(NamenLanguages);
