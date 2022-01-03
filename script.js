f418d66280e2c4eb7304eceaaee90a475ba125ad
sham912411 <shamalhakim91@gmail.com>
2022-01-03 10:49:59 +0100

    Sam

---
+++ script.js
@@ -0,0 +1,8 @@
+const languages= ["Deutsch", "Englisch", "Spanisch", "Französisch", "Italienisch", "Russisch", "Arabisch", "Portugiesisch"];
+
+languages.sort();
+
+console.log(languages);
+languages.sort ();
+languages.reverse ();
+console.log(languages);
