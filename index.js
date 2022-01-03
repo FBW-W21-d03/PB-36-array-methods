cc615380cb1d33c0e4c145bf9c97fb349f7b90dd
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-03 10:15:47 +0100

    übung PB36

---
+++ index.js
@@ -0,0 +1,33 @@
+// array alphabetisch sortiert
+const employees = ["john" , "bob" , "chuck"];
+
+employees.sort();
+
+console.log(employees);
+
+// array mit chartsongs
+const charts = ["bababy" , "great sunshine" , "never been alone" , "good news" , "update in june" , "off party is next"];
+
+charts.sort();
+console.log(charts);
+
+//array rückwärts sortiert
+
+charts.sort();
+charts.reverse();
+console.log(charts);
+
+//--------------- aufgabe 1
+
+const languages = ["Deutsch" , "Englisch" , "Spanisch" , "Französisch" , "Italienisch" , "Russisch" , "Arabisch" , "Portugiesisch"];
+
+// --------------- aufgabe 2
+
+languages.sort();
+console.log(languages);
+
+// -------- aufgabe 3
+
+languages.sort();
+languages.reverse();
+console.log(languages);
