63b4d6e339f0449b878f6193505cb39891e93ec4
Mazen <mazen.taha88@gmail.com>
2022-01-03 10:17:54 +0100

    My Lösung

---
+++ index.js
@@ -0,0 +1,6 @@
+const languages =["Deutsch", "English", "Spanisch", "Französisch", "Italienisch", "Russisch", "Arabisch", "Portugiesisch"]
+
+languages. sort ();
+languages. reverse();
+
+console.log(languages);
