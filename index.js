b7b6628a32d6d6a9d5025120398e0ad672925f38
slavablack <veaceslav.ciornii@magenta.de>
2022-01-03 10:09:44 +0100

    Veaceslav

---
+++ index.js
@@ -0,0 +1,8 @@
+let languages = ["Deutch", "English","Spanish","Französisch", "Italienisch", "Russisch", "Arabisch", "Portugisich"];
+//A2
+languages.sort ();
+console.log(languages);
+//A3
+languages.sort ();
+languages.reverse ();
+console.log(languages);
