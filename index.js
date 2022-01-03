3b78b9a458d64a80c2a494d8383faf7aacf4d263
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-03 10:32:57 +0100

    Meine Lösung

---
+++ index.js
@@ -0,0 +1,17 @@
+// Aufgabenstellung - Teil 1
+const list = ["Deutsch","Englisch"," Spanisch","Französisch","Italienisch","Russisch","Arabisch","Portugiesisch"];
+
+list.sort();
+
+console.log(list);
+
+// AUFGABENTEIL 2 - Array mit Sprachen - RÜCKWERTS
+const sprachen = ["Deutsch","Englisch"," Spanisch","Französisch","Italienisch","Russisch","Arabisch","Portugiesisch"];
+
+// WICHTIG: ERST SORTIEREN!
+sprachen.sort();
+
+// DANN SPIEGELN
+sprachen.reverse();
+
+console.log(sprachen);
