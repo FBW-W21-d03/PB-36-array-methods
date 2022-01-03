af66e7c0bc0c853d17640970c830b36e72c246d1
Serdar68 <Serdarbalkis@outlook.de>
2022-01-03 10:31:27 +0100

    serdar

---
+++ index.js
@@ -0,0 +1,20 @@
+// javascript-array-methods
+
+// Aufgabenstellung - Teil 1
+// Ein Array mit dem Namen languages soll folgende Strings enthalten
+
+let languages = ["Deutsch", "Englisch", "Spanisch", "Französisch", "Italienisch", "Russisch", "Arabisch", "Portugiesisch"]
+
+languages.sort()
+console.log(languages)
+
+
+// Aufgabenstellung - Teil 2
+// Das Array soll nun alphabetisch aufsteigend sortiert werden.
+
+// Aufgabenstellung - Teil 3
+// Das Array soll nun alphabetisch absteigend sortiert werden.
+let languages1 = ["Deutsch", "Englisch", "Spanisch", "Französisch", "Italienisch", "Russisch", "Arabisch", "Portugiesisch"]
+
+languages1.reverse()
+console.log(languages1)
