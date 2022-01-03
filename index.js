3d55a53b8f00151fe8a9e61bb8d4449284fb9f78
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-03 10:24:12 +0100

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+const languages = ["Deutsch", "Englisch", "Spanisch", "Französisch", "Italienisch", "Russisch", "Arabisch",
+"Portugiesisch"];
+languages.sort();
+console.log(languages);
+
+
+
+const language = ["Deutsch", "Englisch", "Spanisch", "Französisch", "Italienisch", "Russisch", "Arabisch",
+"Portugiesisch"];
+language.reverse();
+console.log(language);
