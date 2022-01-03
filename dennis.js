e1ca829bb477abb12e71ca7c5d5c81760d97f2de
Dennisbullys <d.rochus@hotmail.de>
2022-01-03 10:19:41 +0100

    Dennis

---
+++ dennis.js
@@ -0,0 +1,9 @@
+const languages = ["Deutsch","Englisch","Spanisch","Französisch","Italienisch","Russisch","Arabisch","Portugiesisch"]
+
+languages.sort();
+console.log(languages);
+
+// Aufgabe 2
+
+languages.reverse();
+console.log(languages);
